259735cc623407959d3912d00a57154107e35448
Chris Rae <clr@chrisrae.com>
2014-01-24 15:50:49 -0800

    Contour detection and box-drawing

--- src/org/opencv/samples/colorblobdetect/ColorBlobDetectionActivity.java
+++ src/org/opencv/samples/colorblobdetect/ColorBlobDetectionActivity.java
@@ -11,7 +11,10 @@ import org.opencv.core.Core;
 import org.opencv.core.CvType;
 import org.opencv.core.Mat;
 import org.opencv.core.MatOfPoint;
+import org.opencv.core.MatOfPoint2f;
+import org.opencv.core.Point;
 import org.opencv.core.Rect;
+import org.opencv.core.RotatedRect;
 import org.opencv.core.Scalar;
 import org.opencv.core.Size;
 import org.opencv.android.CameraBridgeViewBase;
@@ -27,112 +30,137 @@ import android.view.Window;
 import android.view.WindowManager;
 import android.view.View.OnTouchListener;
 
-public class ColorBlobDetectionActivity extends Activity implements CvCameraViewListener2 {
-    private static final String  TAG              = "OCVSample::Activity";
+public class ColorBlobDetectionActivity extends Activity implements
+		CvCameraViewListener2 {
+	private static final String TAG = "OCVSample::Activity";
 
-    private boolean              mIsColorSelected = false;
-    private Mat                  mRgba;
-    private Mat                  mRgbaPrev;
-    private Scalar               mBlobColorRgba;
-    private Scalar               mBlobColorHsv;
-    private ColorBlobDetector    mDetector;
-    private Mat                  mSpectrum;
-    private Size                 SPECTRUM_SIZE;
-    private Scalar               CONTOUR_COLOR;
+	private boolean mIsColorSelected = false;
+	private Mat mRgba;
+	private Mat mRgbaPrev;
+	private Scalar mBlobColorRgba;
+	private Scalar mBlobColorHsv;
+	private ColorBlobDetector mDetector;
+	private Mat mSpectrum;
+	private Size SPECTRUM_SIZE;
+	private Scalar CONTOUR_COLOR;
 
-    private CameraBridgeViewBase mOpenCvCameraView;
+	private CameraBridgeViewBase mOpenCvCameraView;
 
-    private BaseLoaderCallback  mLoaderCallback = new BaseLoaderCallback(this) {
-        @Override
-        public void onManagerConnected(int status) {
-            switch (status) {
-                case LoaderCallbackInterface.SUCCESS:
-                {
-                    Log.i(TAG, "OpenCV loaded successfully");
-                    mOpenCvCameraView.enableView();
-                } break;
-                default:
-                {
-                    super.onManagerConnected(status);
-                } break;
-            }
-        }
-    };
+	private BaseLoaderCallback mLoaderCallback = new BaseLoaderCallback(this) {
+		@Override
+		public void onManagerConnected(int status) {
+			switch (status) {
+			case LoaderCallbackInterface.SUCCESS: {
+				Log.i(TAG, "OpenCV loaded successfully");
+				mOpenCvCameraView.enableView();
+			}
+				break;
+			default: {
+				super.onManagerConnected(status);
+			}
+				break;
+			}
+		}
+	};
 
-    public ColorBlobDetectionActivity() {
-        Log.i(TAG, "Instantiated new " + this.getClass());
-    }
+	public ColorBlobDetectionActivity() {
+		Log.i(TAG, "Instantiated new " + this.getClass());
+	}
 
-    /** Called when the activity is first created. */
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        Log.i(TAG, "called onCreate");
-        super.onCreate(savedInstanceState);
-        requestWindowFeature(Window.FEATURE_NO_TITLE);
-        getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
+	/** Called when the activity is first created. */
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		Log.i(TAG, "called onCreate");
+		super.onCreate(savedInstanceState);
+		requestWindowFeature(Window.FEATURE_NO_TITLE);
+		getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
 
-        setContentView(R.layout.color_blob_detection_surface_view);
+		setContentView(R.layout.color_blob_detection_surface_view);
 
-        mOpenCvCameraView = (CameraBridgeViewBase) findViewById(R.id.color_blob_detection_activity_surface_view);
-        mOpenCvCameraView.setCvCameraViewListener(this);
-    }
+		mOpenCvCameraView = (CameraBridgeViewBase) findViewById(R.id.color_blob_detection_activity_surface_view);
+		mOpenCvCameraView.setMaxFrameSize(200, 200);
+		mOpenCvCameraView.setCvCameraViewListener(this);
+	}
 
-    @Override
-    public void onPause()
-    {
-        super.onPause();
-        if (mOpenCvCameraView != null)
-            mOpenCvCameraView.disableView();
-    }
+	@Override
+	public void onPause() {
+		super.onPause();
+		if (mOpenCvCameraView != null)
+			mOpenCvCameraView.disableView();
+	}
 
-    @Override
-    public void onResume()
-    {
-        super.onResume();
-        OpenCVLoader.initAsync(OpenCVLoader.OPENCV_VERSION_2_4_3, this, mLoaderCallback);
-    }
+	@Override
+	public void onResume() {
+		super.onResume();
+		OpenCVLoader.initAsync(OpenCVLoader.OPENCV_VERSION_2_4_3, this,
+				mLoaderCallback);
+	}
 
-    public void onDestroy() {
-        super.onDestroy();
-        if (mOpenCvCameraView != null)
-            mOpenCvCameraView.disableView();
-    }
+	public void onDestroy() {
+		super.onDestroy();
+		if (mOpenCvCameraView != null)
+			mOpenCvCameraView.disableView();
+	}
 
-    public void onCameraViewStarted(int width, int height) {
-        mRgba = new Mat(height, width, CvType.CV_8U);
-        mDetector = new ColorBlobDetector();
-        mSpectrum = new Mat();
-        mBlobColorRgba = new Scalar(255);
-        mBlobColorHsv = new Scalar(255);
-        SPECTRUM_SIZE = new Size(200, 64);
-        CONTOUR_COLOR = new Scalar(255,0,0,255);
-    }
+	public void onCameraViewStarted(int width, int height) {
+		mRgba = new Mat(height, width, CvType.CV_8U);
+		mDetector = new ColorBlobDetector();
+		mSpectrum = new Mat();
+		mBlobColorRgba = new Scalar(255);
+		mBlobColorHsv = new Scalar(255);
+		SPECTRUM_SIZE = new Size(200, 64);
+		CONTOUR_COLOR = new Scalar(255, 0, 0, 255);
+	}
 
-    public void onCameraViewStopped() {
-        mRgba.release();
-    }
+	public void onCameraViewStopped() {
+		mRgba.release();
+	}
 
-    public Mat onCameraFrame(CvCameraViewFrame inputFrame) {
-    	// Some significant thievery from https://code.google.com/p/javacv/source/browse/samples/MotionDetector.java?r=02385ce192fb82f1668386e55ff71ed8d6f88ae3
-    	
-        mRgba = inputFrame.rgba();
-        
-        // Make BW version of frame
-        Mat bw = new Mat(mRgba.height(), mRgba.width(), CvType.CV_8UC1);
-        Imgproc.cvtColor(mRgba, bw, Imgproc.COLOR_RGB2GRAY);
+	public Mat onCameraFrame(CvCameraViewFrame inputFrame) {
+		// Some significant thievery from
+		// https://code.google.com/p/javacv/source/browse/samples/MotionDetector.java?r=02385ce192fb82f1668386e55ff71ed8d6f88ae3
 
-        Mat diff = new Mat(mRgba.height(), mRgba.width(), CvType.CV_8UC1);
-        if (mRgbaPrev != null) {
-        	Core.absdiff(bw, mRgbaPrev, diff);
-            Imgproc.threshold(diff, diff, 64, 255, Imgproc.THRESH_BINARY);
-            List<MatOfPoint> contours = new ArrayList<MatOfPoint>();
-            Mat mHierarchy = new Mat(0, 0, CvType.CV_8U);
-            //Imgproc.findContours(diff, contours, mHierarchy, Imgproc.RETR_EXTERNAL, Imgproc.CHAIN_APPROX_SIMPLE);
-            mRgbaPrev.release();
-            //diff.release();
-        }
+		mRgba = inputFrame.rgba();
 
-        mRgbaPrev = bw;
-        return diff; //mRgba;
-    }
+		// Make BW version of frame
+		Mat bw = new Mat(mRgba.height(), mRgba.width(), CvType.CV_8UC1);
+		Imgproc.cvtColor(mRgba, bw, Imgproc.COLOR_RGB2GRAY);
+
+		// Prepare diff frame
+		Mat diff = new Mat(mRgba.height(), mRgba.width(), CvType.CV_8UC1);
+		if (mRgbaPrev != null) {
+			Core.absdiff(bw, mRgbaPrev, diff);
+			// Do some noise reduction
+			Imgproc.threshold(diff, diff, 64, 255, Imgproc.THRESH_BINARY);
+			List<MatOfPoint> contours = new ArrayList<MatOfPoint>();
+			Mat mHierarchy = new Mat(0, 0, CvType.CV_8U);
+			Imgproc.findContours(diff, contours, mHierarchy,
+					Imgproc.RETR_EXTERNAL, Imgproc.CHAIN_APPROX_SIMPLE);
+
+			if (contours.size() > 0) {
+				MatOfPoint2f allPoints = new MatOfPoint2f();
+				// allPoints.push_back(new
+				// MatOfPoint2f(contours.get(0).toArray()));
+				// Bumble through all the contours
+				for (MatOfPoint matOfPoint : contours) {
+					MatOfPoint2f points = new MatOfPoint2f(matOfPoint.toArray());
+					allPoints.push_back(points);
+					// Draw a box around this specific area
+					RotatedRect box = Imgproc.minAreaRect(points);
+					Core.rectangle(mRgba, box.boundingRect().tl(), box
+							.boundingRect().br(), new Scalar(180, 0, 0));
+				}
+				// Draw a box around the whole lot
+				RotatedRect box = Imgproc.minAreaRect(allPoints);
+				Core.rectangle(mRgba, box.boundingRect().tl(), box
+						.boundingRect().br(), new Scalar(0, 0, 250));
+			}
+
+			mRgbaPrev.release();
+			diff.release();
+		}
+
+		mRgbaPrev = bw;
+		return mRgba;
+	}
 }
